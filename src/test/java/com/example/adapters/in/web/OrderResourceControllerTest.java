42cacc52fc28e156b94e11902e41f1daacc41b71
Pablo de Arriba Garcia <pabloaga@ext.inditex.com>
2024-07-17 11:49:54 +0200

    Cambios clase 3

--- src/test/java/com/example/adapters/in/web/OrderResourceControllerTest.java
+++ src/test/java/com/example/adapters/in/web/OrderResourceControllerTest.java
@@ -1,4 +1,28 @@
+package com.example.adapters.in.web;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class OrderResourceControllerTest {
-  
+
+    @Test
+    void getAllOrders() {
+    }
+
+    @Test
+    void createOrder() {
+    }
+
+    @Test
+    void addItemsToOrder() {
+    }
+
+    @Test
+    void updateOrderStatus() {
+    }
+
+    @Test
+    void getOrder() {
+    }
 }
